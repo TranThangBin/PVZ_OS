3223165652fe15eefdff7bdab5c8bf8a4c788d0d
TranThangBin <thangdev04@gmail.com>
2025-05-22 09:03:47 +0700

    feat: Home scene

---
+++ Assets/Scripts/Home.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Home : MonoBehaviour
+{
+    public void NewGame()
+    {
+
+    }
+
+    public void Continue()
+    {
+
+    }
+
+    public void Settings()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit(0);
+    }
+}
