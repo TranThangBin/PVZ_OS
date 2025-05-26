472f7cff9f4d644cecbd509b179b295b6884f81d
TranThangBin <thangdev04@gmail.com>
2025-05-26 15:34:31 +0700

    feat: lawn mower

---
+++ Assets/Scripts/Cleaners/LawnMower.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class LawnMower : MonoBehaviour
+{
+    [SerializeField] private float _velocity;
+    [SerializeField] private float _damage;
+
+    private Rigidbody2D _rb;
+
+    public void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out HealthManager healthManager))
+        {
+            healthManager.ReduceHealth(_damage);
+            if (_rb.linearVelocity == Vector2.zero)
+            {
+                _rb.linearVelocity = _velocity * Vector2.right;
+            }
+        }
+    }
+}
